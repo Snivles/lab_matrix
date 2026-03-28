a74f09f12bd59963cb789007504de8358c72ae74
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-03-28 13:39:46 +0700

    добавление основых функций(освобождения, создания и тд)

--- main.c
+++ main.c
@@ -1,4 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+
+int **CreateMatrix(size_t row, size_t column){
+    int **dmas = (int **)calloc(row, sizeof(int *));
+    if (dmas == NULL){
+        return NULL;
+    }
+
+
+    for (size_t i = 0; i < row; i++) {
+        dmas[i] = (int *)calloc(column, sizeof(int));
+        if (dmas[i] == NULL){
+            for (size_t j = 0; j < i; j++) free(dmas[j]);
+            free(dmas);
+            return NULL;
+        }
+    }
+    return dmas;
+  return NULL;}
+
+
+void PrintMx(int **Mx, size_t row, size_t column)
+{
+    if (Mx == NULL) return;
+    for (size_t ix = 0; ix < row; ix++) {
+        if (Mx[ix]) {
+            for (size_t jx = 0; jx < column; jx++) {
+                printf("%4d ", Mx[ix][jx]);
+            }
+        } else {
+            printf("NULL");
+        }
+        printf("\n");
+    }
+}
+
+void generateRandomMas(int **Mx, size_t row , size_t column, int start , int end){
+  if (Mx == NULL) return;
+  if( start > end){
+    int flag = end;
+    end = start;
+    start = flag;
+}
+  if(start <= end && (start >=0 && end>=0)){
+  for (size_t i = 0; i < row; i++)
+    for (size_t j = 0; j < column; j++)
+        if(Mx[i] != NULL){
+        Mx[i][j] = rand() % (end-start+1)+start;}
+  }
+}
+
+void FreeMx(int ***Mx, size_t rows) {
+    if (Mx == NULL || *Mx == NULL) return;
+
+    for (size_t i = 0; i < rows; i++) {
+        free((*Mx)[i]);
+    }
+    free(*Mx);
+    *Mx = NULL;
+}
 
 int main()
 {
