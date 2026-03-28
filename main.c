12a915448e01ad63e8a9277c46a360838e8d608b
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-03-28 13:37:54 +0700

    подключение библиотек + добавление функций создания матрицы и вывода ее

--- main.c
+++ main.c
@@ -1,4 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+
+int **CreateMatrix(size_t row, size_t column){
+    int **dmas = (int **)calloc(row, sizeof(int *));
+    if (dmas == NULL){
+        return NULL;
+    }
+
+
+    for (size_t i = 0; i < row; i++) {
+        dmas[i] = (int *)calloc(column, sizeof(int));
+        if (dmas[i] == NULL){
+            for (size_t j = 0; j < i; j++) free(dmas[j]);
+            free(dmas);
+            return NULL;
+        }
+    }
+    return dmas;
+  return NULL;}
+
+
+void PrintMx(int **Mx, size_t row, size_t column)
+{
+    if (Mx == NULL) return;
+    for (size_t ix = 0; ix < row; ix++) {
+        if (Mx[ix]) {
+            for (size_t jx = 0; jx < column; jx++) {
+                printf("%4d ", Mx[ix][jx]);
+            }
+        } else {
+            printf("NULL");
+        }
+        printf("\n");
+    }
+}
+
+
+
+
 
 int main()
 {
