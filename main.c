0778ebe4bc79184ada47706381556a4db4704fe8
Golovin Kirill <kirill.golovin.256@gmail.com>
2026-03-28 19:19:53 +0700

    заполнение функции main

--- main.c
+++ main.c
@@ -94,6 +94,64 @@ int **Sum_Mx(int **Mx_A, size_t row_A, size_t column_A,int **Mx_B, size_t row_B,
 
 int main()
 {
-    printf("Hello World!\n");
-    return 0;
+  //int row1 = 2, column1 = 4;
+  //int row2 = 2, column2 = 4; - работает штатно все функции выполнены
+
+
+  //int row1 = 2, column1 = 4;
+  //int row2 = 0, column2 = 4; - работатет штатно сообщаем пользователю что ошибка с размерами
+
+
+  //int row1 = 5, column1 = 4;
+  //int row2 = 2, column2 = 4; - работает штатно тк размеры матрицы различны(не кв матрица) => суммы быть не может а транспозиция выполнена
+
+  //int row1 = 10, column1 = 22;
+  //int row2 = 10, column2 = 22; // - проверка работы матрицы с большими размерами
+
+  //int row1 = 1, column1 = 5;
+  //int row2 = 5, column2 = 1; // - даны 2 матриц вектора работает штатно: суммы нет тк размеры не соответ , транспозиция превращает вектор-столбец
+// в вектор-строку и наоборот
+
+  int row1 = 3, column1 = 3;
+  int row2 = 3, column2 = 3; //- работает штатно все функции выполнены для заданных вектор-строк (сумма тоже является вектор-строкой),
+//транспозиция дает вектор-столбец
+
+  if (row1<=0 || row2 <= 0 || column1 <= 0 || column2 <= 0){printf("Error with size"); return 0;}
+  int start = 5;
+  int end = 100;
+  int **A = CreateMatrix(row1, column1);
+  int **B = CreateMatrix(row2, column2);
+  if (!A || !B){printf("Error with mx");FreeMx(&A,row1);FreeMx(&B,row2); return 0;}
+
+  srand(time(0));
+  generateRandomMas(A,row1,column1,start,end);
+  //generateRandomMas(NULL,row1,column1,start,end);
+
+  generateRandomMas(B,row2,column2,start,end);
+  //generateRandomMas(NULL,row2,column2,start,end);
+
+
+  free(A[1]);
+  A[1] = NULL;// - функция транспозиции отрабатывает штатно, СУММУ РАССМОТРЕТЬ !!!(P.s. Сумма теперть тоже работает)
+
+  free(B[2]);
+  B[2] = NULL;//- функция транспозиции отрабатывает штатно, СУММУ РАССМОТРЕТЬ !!!(P.s. Сумма теперть тоже работает)
+
+  int **C = Sum_Mx(A, row1, column1, B, row2, column2);
+  //int **C = Sum_Mx(NULL, row1, column1, B, row2, column2);
+  //int **C = Sum_Mx(NULL, row1, column1, NULL, row2, column2);
+  PrintMx(A,row1,column1);
+  printf("------------------------\n");
+  PrintMx(B,row2,column2);
+  if(C){
+      printf("------------------------\n");
+      printf("Sum\n");
+      PrintMx(C,row2,column2);
+      FreeMx(&C,row1);}
+  else{
+      printf("------------------------\n");
+      printf("Error with sum\n");}
+  FreeMx(&A,row1);
+  FreeMx(&B,row2);
+  return 0;
 }
